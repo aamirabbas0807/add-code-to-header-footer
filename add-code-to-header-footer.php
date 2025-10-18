32aaba3200e7502af093324466530d7ad2d7e6df
Aamir Abbas <82185427+aamirabbas0807@users.noreply.github.com>
2025-10-19 00:55:52 +0500

    Update add-code-to-header-footer.php

--- add-code-to-header-footer.php
+++ add-code-to-header-footer.php
@@ -1,1 +1,16 @@
+<?php
+/**
+ * Plugin Name: Add Code to Header and Footer
+ * Plugin URI:  
+ * Description: Add custom HTML, CSS, or JavaScript to your site’s header and footer — safely and easily.
+ * Version:     1.0.0
+ * Author:      Aamir Abbas
+ * Author URI:  
+ * License:     GPLv2 or later
+ * Text Domain: add-code-to-header-footer
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Stop execution if accessed directly (security)
+}
 
